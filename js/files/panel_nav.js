bc0713d6e766b64b238ac9f75b25c8bd47eb7e56
crevanwoo <stasya.job@outlook.com>
2017-02-08 20:02:12 +0200

    little_fixes

--- js/files/panel_nav.js
+++ js/files/panel_nav.js
@@ -1,6 +1,12 @@
 $('.content_nav .nav_main .type').on('click', function () {
     $('.content_nav .nav_main .type').removeClass('active');
     $(this).addClass('active');
+})
 
-
+$('.tab_2').on('click', function () {
+    $(this).toggleClass('active');   
+    
+    
+    
+    
 })
--- js/script.js
+++ js/script.js
@@ -41,8 +41,14 @@ $(window).on("wheel keydown touchstart touchmove", function () {
 $('.content_nav .nav_main .type').on('click', function () {
     $('.content_nav .nav_main .type').removeClass('active');
     $(this).addClass('active');
+})
 
-
+$('.tab_2').on('click', function () {
+    $(this).toggleClass('active');   
+    
+    
+    
+    
 })
 
 $('.sort_az .az_trigger').on('click', function () {
